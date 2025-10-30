3492b7bc2c399ef13565e1448f7b3a066788a6a8
Dean James <deanjamesgreeff@gmail.com>
2025-10-30 21:04:15 +0200

    Created Frontend Views
    - Using HTMX
    - Partial Views
    - Bootstrap

---
+++ HeartSyncSolutions/Controllers/DonationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeartSyncSolutions.Controllers
+{
+    public class DonationController : Controller
+    {
+        [HttpGet]
+        public IActionResult DonationPartial()
+        {
+            Console.WriteLine("DONATION PARTIAL");
+            return PartialView("_Donation");
+        }
+    }
+}
---
+++ HeartSyncSolutions/Controllers/EventController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HeartSyncSolutions.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult EventListPartial()
+        {
+            return PartialView("_EventList");
+        }
+
+        public IActionResult CreateEventPartial()
+        {
+            return PartialView("_CreateEvent");
+        }
+
+        public IActionResult EventDetailsPartial()
+        {
+            return PartialView("_EventDetails");
+        }
+
+        public IActionResult SignedUpEventsPartial()
+        {
+            return PartialView("_SignedUpEvents");
+        }
+    }
+}
