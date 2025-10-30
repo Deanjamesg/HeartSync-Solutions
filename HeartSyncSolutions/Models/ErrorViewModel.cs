416e57a0bb1c2c76b9d166cb88b7511140d149c7
Dean James <deanjamesgreeff@gmail.com>
2025-10-30 17:07:37 +0200

    Project Initial Create
    Web Application:
    - C#
    - ASP.NET MVC
    - Microsoft Entity Framework
    - Microsoft Identity
    - Bootstrap

---
+++ HeartSyncSolutions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HeartSyncSolutions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
