15141c74109ad2c92c6b6b2ea215f2f6ee2f7386
Dean James <deanjamesgreeff@gmail.com>
2025-10-31 16:54:40 +0200

    Frontend/Donation
    Created Views:
    - Creating donation (In-Kind & Monetary)
    - Viewing donation history
    - Viewing all donations
    - Viewing & processing donation requests

--- HeartSyncSolutions/Controllers/DonationController.cs
+++ HeartSyncSolutions/Controllers/DonationController.cs
@@ -1,14 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HeartSyncSolutions.Controllers
 {
+    [AllowAnonymous]
     public class DonationController : Controller
     {
-        [HttpGet]
-        public IActionResult DonationPartial()
+        private readonly ILogger<DonationController> _logger;
+
+        public DonationController(ILogger<DonationController> logger)
         {
-            Console.WriteLine("DONATION PARTIAL");
-            return PartialView("_Donation");
+            _logger = logger;
         }
+
+        public IActionResult Donate()
+        {
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_Donate");
+            }
+
+            return View("_Donate");
+        }
+
+        public IActionResult SubmitDonation()
+        {
+            if (Request.Headers["HX-Request"] == "true")
+            {
+            }
+
+            return RedirectToAction("DonationHistory");
+        }
+
+        public IActionResult RequestInKindDonation()
+        {
+            return RedirectToAction("DonationHistory");
+        }
+
+        public IActionResult DonationHistory()
+        {
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_DonationHistory");
+            }
+
+            return View("_DonationHistory");
+        }
+
+        public IActionResult DonationsList()
+        {
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_DonationsList");
+            }
+
+            return View("_DonationsList");
+        }
+
+        public IActionResult DonationRequests()
+        {
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_DonationRequests");
+            }
+
+            return View("_Donate");
+        }
+
     }
 }
