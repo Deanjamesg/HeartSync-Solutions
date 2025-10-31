cdc1a0f200b4aa20e65d021691ab30501dc580db
Dean James <deanjamesgreeff@gmail.com>
2025-10-31 19:57:52 +0200

    Frontend/Events
    Views Created:
    - Create Event
    - Manage Events
    - View Event Details
    - View Signed Up Events

--- HeartSyncSolutions/Controllers/EventController.cs
+++ HeartSyncSolutions/Controllers/EventController.cs
@@ -4,24 +4,102 @@ namespace HeartSyncSolutions.Controllers
 {
     public class EventController : Controller
     {
-        public IActionResult EventListPartial()
+        private readonly ILogger<EventController> _logger;
+
+        public EventController(ILogger<EventController> logger)
         {
-            return PartialView("_EventList");
+            _logger = logger;
         }
 
-        public IActionResult CreateEventPartial()
+        public IActionResult Events()
         {
-            return PartialView("_CreateEvent");
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_Events");
+            }
+
+            return View("_Events");
         }
 
-        public IActionResult EventDetailsPartial()
+        public IActionResult EventDetails(string eventId)
         {
-            return PartialView("_EventDetails");
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_EventDetails");
+            }
+
+            return View("_EventDetails");
         }
 
-        public IActionResult SignedUpEventsPartial()
+        public IActionResult SignedUpEvents()
         {
-            return PartialView("_SignedUpEvents");
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_SignedUpEvents");
+            }
+
+            return View("_SignedUpEvents");
         }
+
+        public IActionResult CreateEvent()
+        {
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_CreateEvent");
+            }
+
+            return View("_CreateEvent");
+        }
+
+        public IActionResult EventManager()
+        {
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_EventManager");
+            }
+
+            return View("_EventManager");
+        }
+
+        public IActionResult SignUp(string eventId)
+        {
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return Ok(new { success = true, message = "Successfully signed up for event!" });
+            }
+
+            return RedirectToAction("SignedUpEvents");
+        }
+
+        public IActionResult CancelRegistration(string eventId)
+        {
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_SignedUpEvents");
+            }
+
+            return RedirectToAction("SignedUpEvents");
+        }
+
+        public IActionResult Delete(string eventId)
+        {
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_EventManager");
+            }
+
+            return RedirectToAction("EventManager");
+        }
+
+        public IActionResult Edit(string eventId)
+        {
+            if (Request.Headers["HX-Request"] == "true")
+            {
+                return PartialView("_CreateEvent");
+            }
+
+            return View("_CreateEvent");
+        }
+
     }
 }
